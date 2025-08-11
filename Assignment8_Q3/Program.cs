8c6dd2a1c7c28a9f4f9ed9d5371cec5654acca4d
prudhvi babu <prudhvibabu9@gmail.com>
2025-08-11 10:00:57 +0530

    Add files via upload

---
+++ Assignment8_Q3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment8_Q3;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] A = { 5, 3, -6, -4, 10, 2 };
+
+
+        SolutionQ3 sol = new SolutionQ3();
+        int result = sol.solution(A);
+
+
+        Console.WriteLine("Sum = " + result);
+    }
+}
---
+++ Assignment8_Q3/SolutionQ3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_Q3
+{
+    internal class SolutionQ3
+    {
+        public int solution(int[] A)
+        {
+
+            int sum = 0;
+
+            foreach (int number in A)
+            {
+                if (number == 0)
+                    break;
+
+                if (number > 0)
+                    sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
+// Algorithm for Assignment8_Q3:
+
+// 1. Initialize sum = 0.
+// 2. Loop through each element in the array:
+//    - If element == 0, stop processing (break).
+//    - If element > 0, add it to the sum.
+//    - Skip negative numbers.
+// 3. Return the final sum.
