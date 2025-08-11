19432e42cfad7d50321a1e48c4db185cabe185f7
prudhvi babu <prudhvibabu9@gmail.com>
2025-08-11 09:59:38 +0530

    Add files via upload

---
+++ Assignment7/Pizza.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    internal class Pizza
+    {
+        // Private instance variables
+        private string size;
+        private int cheeseToppings;
+        private int pepperoniToppings;
+        private int hamToppings;
+
+        // Constructor
+        public Pizza(string size, int cheeseToppings, int pepperoniToppings, int hamToppings)
+        {
+            this.size = size;
+            this.cheeseToppings = cheeseToppings;
+            this.pepperoniToppings = pepperoniToppings;
+            this.hamToppings = hamToppings;
+        }
+
+        // Get and Set methods
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public int CheeseToppings
+        {
+            get { return cheeseToppings; }
+            set { cheeseToppings = value; }
+        }
+
+        public int PepperoniToppings
+        {
+            get { return pepperoniToppings; }
+            set { pepperoniToppings = value; }
+        }
+
+        public int HamToppings
+        {
+            get { return hamToppings; }
+            set { hamToppings = value; }
+        }
+
+        // calcCost method
+        public double CalcCost()
+        {
+            int totalToppings = cheeseToppings + pepperoniToppings + hamToppings;
+            double basePrice = 0;
+
+            switch (size.ToLower())
+            {
+                case "small":
+                    basePrice = 10;
+                    break;
+                case "medium":
+                    basePrice = 12;
+                    break;
+                case "large":
+                    basePrice = 14;
+                    break;
+                default:
+                    Console.WriteLine("Invalid size. Setting base price to 0.");
+                    basePrice = 0;
+                    break;
+            }
+
+            return basePrice + (2 * totalToppings);
+        }
+
+        // getDescription method
+        public string GetDescription()
+        {
+            return $"Size: {size}, Cheese: {cheeseToppings}, Pepperoni: {pepperoniToppings}, Ham: {hamToppings}, Total Cost: ${CalcCost()}";
+        }
+    }
+}
---
+++ Assignment7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment7
+{
+    class Program
+    {
+        static void Main()
+        {
+            // List to store Pizza objects
+            List<Pizza> pizzaOrders = new List<Pizza>();
+
+            // Add sample records
+            pizzaOrders.Add(new Pizza("Small", 1, 1, 1));
+            pizzaOrders.Add(new Pizza("Medium", 2, 2, 1));
+            pizzaOrders.Add(new Pizza("Large", 3, 3, 3));
+
+            // Display records
+            Console.WriteLine("Pizza Orders:");
+            foreach (Pizza p in pizzaOrders)
+            {
+                Console.WriteLine(p.GetDescription());
+            }
+        }
+    }
+}
